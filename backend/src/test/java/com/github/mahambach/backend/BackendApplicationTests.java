34b3cfe647691e4c5b56e49d1c854237f04fdcd6
MaHambach <ma.hambach@gmx.net>
2024-03-18 17:12:06 +0100

    Setup backend

---
+++ backend/src/test/java/com/github/mahambach/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mahambach.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
