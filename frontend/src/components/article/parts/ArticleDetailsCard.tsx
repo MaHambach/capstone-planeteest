5eecfc8923f53e015440bff8c6039544eace484a
MaHambach <ma.hambach@gmx.net>
2024-03-20 17:11:09 +0100

    Add ArticleDetailsCard.css
    Clicking on a map marker on the world map should now display its article. Clicking on the world map should make it vanish again.

--- frontend/src/components/article/parts/ArticleDetailsCard.tsx
+++ frontend/src/components/article/parts/ArticleDetailsCard.tsx
@@ -1,4 +1,7 @@
+import './ArticleDetailsCard.css'
 import {Article} from "../../../types/Article.ts";
+import DisplayTileGallery from "../../parts/DisplayTileGallery.tsx";
+import React from "react";
 
 type ArticleDetailsCardProps = {
     article:Article;
@@ -7,6 +10,14 @@ type ArticleDetailsCardProps = {
 export default function ArticleDetailsCard(props:Readonly<ArticleDetailsCardProps>):React.ReactElement {
 
     return (
-        <h2>Kartoffel</h2>
+        <div className={"articleDetailsCard"}>
+            <article>{props.article.content}</article>
+            <DisplayTileGallery
+                urlPrefix={"/npc/"}
+                tileData={props.article.npcIds.map((npcId:string) => ({id:npcId, name:"Jürgen"}))} /* TODO: get the npc name, when there are npcs. */
+                addNewName={"Neuer Nsc"}
+                addNewUrl={"/npc/new"}
+            />
+        </div>
     )
 }
