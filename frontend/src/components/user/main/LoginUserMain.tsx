09cf4f95235676de72f5e160ce13b78bd1202259
MaHambach <ma.hambach@gmx.net>
2024-04-15 15:35:31 +0200

    Refactor LoginUserMain.tsx.

--- frontend/src/components/user/main/LoginUserMain.tsx
+++ frontend/src/components/user/main/LoginUserMain.tsx
@@ -2,12 +2,34 @@ import './LoginUserMain.css';
 import React, {useState} from "react";
 import {AppUserRegister, emptyAppUserRegister} from "../../../types/AppUserRegister.ts";
 import {useNavigate} from "react-router-dom";
+import Typography from "@mui/joy/Typography";
+import {MdMenuBook} from "react-icons/md";
+import {IconContext} from "react-icons";
+import {GiAnvil} from "react-icons/gi";
+
+const style = {
+    button: {
+        width: 64,
+        height: 64,
+        top: 10,
+        right: 10,
+        padding: 0,
+        border: "1px solid black",
+        zIndex: 1000
+    },
+    iconContext: {
+        "size": "64px",
+    },
+    icon: {
+        "color": "#B87333",
+        filter: "drop-shadow(0 0 8px white)"
+    }
+};
 
 type LoginUserMainProps = {
     loginAppUser: (appUserRegister:AppUserRegister) => Promise<void>;
     registerAppUser: (appUserRegister:AppUserRegister) => void;
 }
-
 export default function LoginUserMain(props:Readonly<LoginUserMainProps>):React.ReactElement {
     const [formData, setFormData] = useState(emptyAppUserRegister);
 
@@ -38,7 +60,21 @@ export default function LoginUserMain(props:Readonly<LoginUserMainProps>):React.
 
     return (
         <main className={"loginUserMain"}>
-            <h1>PlaneTeest</h1>
+
+            IoPin
+
+            <Typography level={"h1"}
+                        sx={{ textShadow: '2px 2px 8px rgba(255, 0, 0, 0.25), -2px -2px 4px rgba(0, 0, 0, 0.25)' }}
+            >
+                Plane
+                <IconContext.Provider value={style.iconContext}>
+                    <div>
+                        <GiAnvil style={style.icon}/>
+                    </div>
+                </IconContext.Provider>
+                Teest
+            </Typography>
+
             <form className={"inputFormForm"} onSubmit={handleLogin}>
                 <div>
                     <input name={"username"} placeholder={"Username"} type={"text"} value={formData.username} onChange={handleChange}/>
